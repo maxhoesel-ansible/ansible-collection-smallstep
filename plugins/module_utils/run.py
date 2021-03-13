cef7007f1d18252be50f3ca2eacc92b0a6c27a70
Max Hösel <git@maxhoesel.de>
2021-03-14 00:46:47 +0100

    refactor: reworked and renamed old modules
    This commit reworks and renames the ca_provisioner and ca_claims modules
    to be more in line with future modules. Reusable fragments have been
    split off and the naming has been adjusted to reflect the underlying
    step-cli commands.

---
+++ plugins/module_utils/run.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2021, Max Hösel <ansible@maxhoesel.de>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+def run_step_cli_command(cli_executable, cli_command, module, result, params=None):
+    """Run a step-cli command on the remote host. Supports check_mode
+    Parameters:
+        - cli_executable: Name or path to the step-cli executable
+        - cli_command: The command to run (e.g. ["ca", "bootstrap"])
+        - module: Current module object
+        - result: dict containing result values to be passed to exit_json()
+        - params: Mapping of module parameters to their command-line arg equivalent. Each parameter
+          is processed according to its type in the argspec:
+            - str/path/int/float/raw/bytes are passed as a value to their mapped parameter (e.g. --parameter=value)
+            - bool only passes the mapped parameter (e.g. --force)
+            - list causes the mapped parameter to be repeated for each value (e.g. --in=1 --in=2)
+          The values for each parameter are read from module.params
+
+    Example for params: {
+        "module_arg_1": "--cli-arg",
+        "module_arg_2": "--different-arg"
+    }
+    """
+    args = [cli_executable]
+    args.extend(cli_command)
+    if not params:
+        params = []
+    for param in params:
+        if module.params[param]:
+            if module.argument_spec[param].get("type", "str") in ["str", "int", "float", "path", "raw", "bytes"]:
+                args.append("{param}={val}".format(param=params[param], val=str(module.params[param])))
+            elif module.argument_spec[param].get("type", "str") == "bool":
+                args.append(params[param])
+            elif module.argument_spec[param].get("type", "str") == "list":
+                for item in module.params[param]:
+                    args.append("{param}={val}".format(param=params[param], val=str(item)))
+
+    if not module.check_mode:
+        rc, result["stdout"], result["stderr"] = module.run_command(args)
+        if rc != 0:
+            result["msg"] = "Error running command {cmd}".format(cmd=args)
+            module.fail_json(**result)
+    return result
