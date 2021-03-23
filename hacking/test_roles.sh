9933681f8e1bfe90a9b43d5e5575b8cc4541d344
Max Hösel <git@maxhoesel.de>
2021-03-23 02:27:12 +0100

    test: install collection user-wide for role tests
    The new step_acme_cert role will use modules from our
    collection as part of its tasks. This means that a
    version of the collection needs to be available for
    the molecule tests to execute correctly.
    
    Unfortunatley, molecule really doesn't like to pick up
    on our project-local install in ansible_collections, even
    with ANSIBLE_COLLECTIONS_PATH configured. I spent a couple
    of hours trying to get molecule to play along, but failed.
    
    So, as a workaround, we can just install the collection
    into the user-wide install directory (~/.ansible/collections),
    which is picked up by molecule just fine. The collection is
    of course removed by cleanup.sh after testing concludes.
    
    This shouldn't have any negative side-effects, unless
    someone launches another ansible job using our collection
    while tests are currently running as the same user - a rather
    unlikely scenario. So while it would be nice to get molecule to
    play along, this workaround should be fine for the forseeable future.

--- hacking/test_roles.sh
+++ hacking/test_roles.sh
@@ -8,7 +8,14 @@ cleanup() {
 
 trap cleanup err exit
 
-ansible-lint -v
+./hacking/build.sh
+
+# Molecule doesn't seem to pick up on the collection install in ansible_collections,
+# even with ANSIBLE_COLLECTIONS_PATH set. As a workaround, install the collection
+# into the user collection directory.
+ansible-galaxy collection install maxhoesel-smallstep-*.tar.gz
+
+ansible-lint -vv
 
 for role in roles/*; do
     (cd "$role" && molecule test)
