a3e92df96495d01cec51a930d615ce57a72f49c7
Max Hösel <git@maxhoesel.de>
2021-03-26 15:38:01 +0100

    test: also remove virtualenv during cleanup

--- hacking/cleanup.sh
+++ hacking/cleanup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf ansible_collections
+rm -rf hacking/venv
 # Need to install the collection in ~/.ansible to test roles, see test_roles.sh
 rm -rf ~/.ansible/collections/ansible_collections/maxhoesel/smallstep
 
