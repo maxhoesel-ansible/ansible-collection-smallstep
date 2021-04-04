fe9393ecd559ccfae79b4b2d5a99ec083879b6ec
Max Hösel <git@maxhoesel.de>
2021-04-04 14:06:48 +0200

    test: run test.sh script for molecule roles

--- hacking/test_roles.sh
+++ hacking/test_roles.sh
@@ -13,10 +13,18 @@ trap cleanup err exit
 # Molecule doesn't seem to pick up on the collection install in ansible_collections,
 # even with ANSIBLE_COLLECTIONS_PATH set. As a workaround, install the collection
 # into the user collection directory.
-ansible-galaxy collection install maxhoesel-smallstep-*.tar.gz
+ansible-galaxy collection install maxhoesel-smallstep*.tar.gz
 
 ansible-lint -vv
 
 for role in roles/*; do
-    (cd "$role" && molecule test)
+    # Try to run test.sh if it exists, run molecule test if not
+    (
+        cd "$role"
+        if [[ -f ./molecule/test.sh ]]; then
+            ./molecule/test.sh
+        else
+            molecule test
+        fi
+    )
 done
