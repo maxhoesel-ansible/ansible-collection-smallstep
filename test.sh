5af293e4fb0222d5306c98b5f2b1fa0c310535d0
Max Hösel <git@maxhoesel.de>
2021-04-07 19:52:26 +0200

    test: switch to tox for testing

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+
+# Lint ansible roles and git commits
+tox -e lint
+
+# Sanity checks for our modules
+tox -e sanity -- --docker --color -v --python 3.6
+
+# Integration tests for modules - we don't have an unit tests as of now
+tox -e integration -- --docker --color -v --python 3.6
+
+# Molecule tests - this grabs all molecule scenarios and executes them.
+# Note that to run this command, you need to have
+# - run and tox command at least once (tox -l is fine)
+# - have the collection built and installed locally.
+#   Running the module tests will do that for you
+tox -e "$(tox -l | grep ansible | grep -v "lint" | tr '\n' ',')"
