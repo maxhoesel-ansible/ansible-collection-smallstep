693374733ee35665abdd12d41a63704568ccb884
Max Hösel <git@maxhoesel.de>
2026-08-03 21:24:47 +0200

    switch to uv for testing

--- tests/plugins/test_plugins.py
+++ tests/plugins/test_plugins.py
@@ -1,18 +1,13 @@
 # pylint: disable=redefined-outer-name
 
-def test_plugins_sanity(collection_test_env, test_versions):
+from tests.conftest import TestOptions
+
+
+def test_plugins_sanity(collection_test_env, test_options: TestOptions):
     params = [
         "ansible-test",
         "sanity", "--docker", "--color", "-v",
-        "--python", test_versions.node_python_version,
+        "--python", test_options.node_python_version,
     ]
-
-    if int(test_versions.ansible_version.split(".")[1]) <= 16:
-        # these flags are only valid with ansible-test 2.16 and older, as they refer to python 2.7.
-        # Do not include them with newer versions of ansible-test
-        params.extend([
-            "--skip-test", "metaclass-boilerplate",
-            "--skip-test", "future-import-boilerplate"
-        ])
 
     collection_test_env.run(params)
--- tests/roles/test_molecule.py
+++ tests/roles/test_molecule.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import pytest
 
+from tests.conftest import TestOptions
+
 # List of molecule scenario directories
 MOLECULE_SCENARIOS = [
     subdir
@@ -16,10 +18,13 @@ def scenario_id(path: Path) -> str:
 
 
 @pytest.mark.parametrize("scenario", MOLECULE_SCENARIOS, ids=scenario_id)
-def test_scenario(scenario: Path, collection_test_env) -> None:
+def test_scenario(scenario: Path, collection_test_env, test_options: TestOptions) -> None:
     role_dir = collection_test_env.cwd / scenario.parent.parent
+    molecule_args = ["molecule", "test", "-s", scenario.name]
+    if test_options.molecule_skip_destroy:
+        molecule_args.extend(["--destroy", "never"])
 
     # Apparently molecule needs this to pick up on project-level config files in .config/molecule 🤷
     # https://github.com/ansible/molecule/blob/e6d63adea6be74a8548dab30ba00bf8474d6c088/src/molecule/util.py#L339
     collection_test_env.run(["git", "init"])
-    collection_test_env.run(["molecule", "test", "-s", scenario.name], cwd=role_dir)
+    collection_test_env.run(molecule_args, cwd=role_dir)
